9b3309e9d80f1a689a0c0aee5678fd17f5adfd46
James Cox <jamesncox@yahoo.com>
2019-04-15 18:00:40 +0000

    Automatically backed up by Learn

--- bin/run.rb
+++ bin/run.rb
@@ -1,15 +1,7 @@
 require_relative '../config/environment'
 
-"Hashie"
-"Sinatra", "1.4.4"
-"Octokit", "~>2.0"
-"awesome_print", :git => 'git@github.com:awesome-print/awesome_print.git'
-
-'Pry', :group => "development"
-
-group: test do
-  gem "rspec"
-end
-
-source "https://rubygems.org"
-
+Hashie
+Sinatra
+Octokit
+Pry
+AwesomePrint
